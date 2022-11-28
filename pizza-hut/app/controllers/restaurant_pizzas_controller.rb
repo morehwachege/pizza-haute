e9345cfba3579148ebc696e89a6ff4869d596fc7
Muriithi Gakuru <antony123muriithi@gmail.com>
2022-11-28 15:21:16 +0300

    add: restaurant pizzas routes

--- pizza-hut/app/controllers/restaurant_pizzas_controller.rb
+++ pizza-hut/app/controllers/restaurant_pizzas_controller.rb
@@ -1,4 +1,7 @@
 class RestaurantPizzasController < ApplicationController
+  wrap_parameters format: []
+rescue_from ActiveRecord::RecordInvalid, with: :rescue_record_invalid
+
   def index
     rest_pizza = RestaurantPizza.all 
     render json: rest_pizza, except: [:created_at, :updated_at], status: :ok
@@ -6,7 +9,7 @@ class RestaurantPizzasController < ApplicationController
 
   def create
     rest_pizza = RestaurantPizza.create!(rest_pizza_params)
-
+    render json: rest_pizza, except: [:created_at, :updated_at], status: :created
   end
 
 
@@ -14,6 +17,10 @@ class RestaurantPizzasController < ApplicationController
 
   def rest_pizza_params
     params.permit(:price, :pizza_id, :restaurant_id)
+  end
+
+  def rescue_record_invalid
+    render json: {errors: ["validation errors"]}, status: :unprocessable_entity
   end
 
 end
--- pizza-hut/app/models/restaurant_pizza.rb
+++ pizza-hut/app/models/restaurant_pizza.rb
@@ -2,5 +2,5 @@ class RestaurantPizza < ApplicationRecord
     belongs_to :pizza
     belongs_to :restaurant
 
-    validates :price, numericality: { greater_than_or_equal_to: 30, less_than_or_equal_to: 1 }
+    validates :price, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 30 }
 end
